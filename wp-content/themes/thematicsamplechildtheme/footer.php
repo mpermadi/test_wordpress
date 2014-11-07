5c6e4c1d5bc1bf55259e3aa51e7fcd8477cec061
Marcelino Permadi <mpermadi@indo-soft.com>
2014-11-06 22:29:09 -0500

    edit with statis footer

--- wp-content/themes/thematicsamplechildtheme/footer.php
+++ wp-content/themes/thematicsamplechildtheme/footer.php
@@ -1,55 +1,66 @@
 <?php
 /**
- * Footer Template
+ * The template for displaying the footer
  *
- * This template closes #main div and displays the #footer div.
- * 
- * Thematic Action Hooks: thematic_abovefooter thematic_belowfooter thematic_after
- * Thematic Filters: thematic_close_wrapper can be used to remove the closing of the #wrapper div
- * 
- * @package Thematic
- * @subpackage Templates
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
  */
 ?>
-		<?php // action hook for placing content above the closing of the #main div
-			thematic_abovemainclose();
-		?>
-		
-		</div><!-- #main -->
-    	
-    	<?php
-			// action hook for placing content above the footer
-			thematic_abovefooter();
-		
-			// Filter provided for altering output of the footer opening element
-    		echo ( apply_filters( 'thematic_open_footer', '<div id="footer">' ) );
-    	?>	
-        	
-        	<?php
-        		// action hook creating the footer 
-        		thematic_footer();
-        	?>
-        	
-		<?php
-			// Filter provided for altering output of the footer closing element
-    		echo ( apply_filters( 'thematic_close_footer', '</div><!-- #footer -->' . "\n" ) );
-   
-   			// action hook for placing content below the footer
-			thematic_belowfooter();
-    	?>
-    	
-	<?php
-		// Filter provided for altering output of wrapping element follows the body tag  
-    	if ( apply_filters( 'thematic_close_wrapper', true ) ) 
-    		echo ( '</div><!-- #wrapper .hfeed -->' . "\n" );
-	
 
-		// action hook for placing content before closing the BODY tag
-		thematic_after(); 
-		
-		// calling WordPress' footer action hook
-		wp_footer();
-	?>
+	<center>
+		<span class="i10">
+			<span style="font-size:11px">
+				<a class="hl" href="/">Home</a>
+				  |  
+				<a class="hl" href="/about.html">About</a>
+				  |  
+				<a class="hl" href="/contact/">Contact Us</a>
+				  |  
+				<a class="hl" href="/store/">Store</a>
+				  |  
+				<a class="hl" href="/acr.html">Certified Retailers</a>
+				  |  
+				<a class="hl" href="/advertising.html">Advertise</a>
+				  |  
+				<a class="hl" href="/faq.html">FAQ</a>
+				  |  
+				<a class="hl" href="/friends.html">Friends</a>
+				  |  
+				<a class="hl" href="/sitemap.html">SiteMap</a>
+				  |  
+				<a class="hl" href="/privacy.html">Privacy</a>
+				  |  
+				<a class="hl" href="/tos.html">TOS</a>
+				  |  
+				<a class="hl" href="/deals/">Deals & Coupons</a>
+			</span>
+		</span>
+		<br>
+		<span class="i10">© 2009-2014, AmmoSeek, LLC   |  Data b2vov8ws54 last updated on 2014-11-06 @ 21:39:20 EST</span>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-5694615-9']);
+			_gaq.push(['_trackPageview']);
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+			})();
+		</script>
+		<script type="text/javascript">
+			var _qevents = _qevents || [];
+			(function() {
+			var elem = document.createElement('script');
+			elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+			elem.async = true;
+			elem.type = "text/javascript";
+			var scpt = document.getElementsByTagName('script')[0];
+			scpt.parentNode.insertBefore(elem, scpt);
+			})();
+			_qevents.push({qacct:"p-nbAnCwVkuc8j9"});
+		</script>
+		<noscript> <div style="display:none;"> <img src="//pixel.quantserve.com/pixel/p-nbAnCwVkuc8j9.gif" border="0" height="1" width="1" alt="Quantcast"/> </div> </noscript>
+		<br>
+		<span style="color:white">SERVER: ammoseek1</span>
+	</center>
 
-</body>
-</html>
